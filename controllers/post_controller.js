5c7e7acbb9d313f85e1d2b3233349c4129dae9e5
AnupamKumar-1 <anupam9594.kumar@gmail.com>
2023-04-25 19:38:22 +0530

    add a section for post form in home.ejs

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,19 +1,22 @@
 const Post = require('../models/post');
 
-module.exports.create = function(req,res){
-Post.create({
-    content : req.body.content,
-    user : req.body._id  
-},
-
-   function(err,post){
-    if(err){
-        console.log('error in creating post');
-        return;
+module.exports.create = async function(req, res) {
+    try {
+        const post = await Post.create({
+            content: req.body.content,
+            user: req.body._id
+        });
+        return res.redirect('back');
+    } catch (err) {
+        console.log('error in creating post', err);
+        return ;
     }
-    
-    return res.redirect('back');
+};
 
-});
 
-}
+
+
+
+
+
+
